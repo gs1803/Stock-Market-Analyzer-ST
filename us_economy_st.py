bddda0e5da42f023b1f7ea1fa7a1daad1654637f
gs1803 <98928448+gs1803@users.noreply.github.com>
2023-06-03 12:23:34 -0500

    Update us_economy_st.py

--- us_economy_st.py
+++ us_economy_st.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from fredapi import Fred
+from stock_analyzer_st import StockAnalyzer
 
 fred = Fred(api_key = st.secrets['API_KEY'])
 
@@ -32,11 +33,15 @@ class USEconomy:
             inflationDf = pd.DataFrame(USEconomy.inflationData)
             inflationDf.index = pd.to_datetime(inflationDf.index)
             inflationDf.columns = ['inflation_rate']
+            StockAnalyzer.arrow_change(inflationDf, 'inflation_rate')
+
             fig = go.Figure(data = go.Scatter(x = inflationDf.index, y = inflationDf['inflation_rate']))
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'Inflation Rate')
-            st.subheader(f"Latest Inflation Rate: {inflationDf['inflation_rate'].iloc[-1]:.3f}%")
+            st.markdown(f"### Latest Inflation Rate: {inflationDf['inflation_rate'].iloc[-1]:.3f}% \
+                        {StockAnalyzer.arrow_change(inflationDf, 'inflation_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
         elif inflationOption == 'Core Inflation Rate':
             coreInflationDf = pd.DataFrame(USEconomy.coreInflationData)
@@ -46,7 +51,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'Core Inflation Rate')
-            st.subheader(f"Latest Core Inflation Rate: {coreInflationDf['core_inflation_rate'].iloc[-1]:.3f}%")
+            st.markdown(f"### Latest Core Inflation Rate: {coreInflationDf['core_inflation_rate'].iloc[-1]:.3f}% \
+                        {StockAnalyzer.arrow_change(coreInflationDf, 'core_inflation_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
     def unemployment_rate() -> None:
@@ -57,7 +64,9 @@ class USEconomy:
 
         fig.update_layout(xaxis_title = 'Date',
                           title = 'Unemployment Rate')
-        st.subheader(f"Latest Unemployment Rate: {unemploymentDf['unemployment_rate'].iloc[-1]:.3f}%")
+        st.markdown(f"### Latest Unemployment Rate: {unemploymentDf['unemployment_rate'].iloc[-1]:.3f}% \
+                    {StockAnalyzer.arrow_change(unemploymentDf, 'unemployment_rate')}",
+                    unsafe_allow_html = True)
         st.plotly_chart(fig, use_container_width = True)
     
     def interest_rates() -> None:
@@ -73,7 +82,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'Federal Funds Effective Rate')
-            st.subheader(f"Latest Federal Funds Effective Rate: {fferDf['ffer'].iloc[-1]:.2f}%")
+            st.markdown(f"### Latest Federal Funds Effective Rate: {fferDf['ffer'].iloc[-1]:.2f}% \
+                        {StockAnalyzer.arrow_change(fferDf, 'ffer')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
         if interestOption == 'Market Yield on U.S. Treasury Securities (10 Year)':
@@ -84,7 +95,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date', 
                               title = 'Market Yield on U.S. Treasury Securities (10 Year) Rate')
-            st.subheader(f"Latest Market Yield on U.S. Treasury Securities (10 Year) Rate: {sofrDf['myuts'].iloc[-1]:.2f}%")
+            st.markdown(f"### Latest Market Yield on U.S. Treasury Securities (10 Year) Rate: {sofrDf['myuts'].iloc[-1]:.2f}% \
+                        {StockAnalyzer.arrow_change(sofrDf, 'myuts')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
         if interestOption == 'Secured Overnight Financing Rate':
@@ -95,44 +108,28 @@ class USEconomy:
             sofr30Df = pd.DataFrame(USEconomy.sofr30Data)
             sofr30Df.index = pd.to_datetime(sofr30Df.index)
             sofr30Df.columns = ['sofr30']
-            sofr30Df['30-day Arrow'] = np.where(sofr30Df['sofr30'] > sofr30Df['sofr30'].shift(), '▲', 
-                                                np.where(sofr30Df['sofr30'] < sofr30Df['sofr30'].shift(), '▼', '▬'))
-            arrowColor30 = np.where(sofr30Df['sofr30'] > sofr30Df['sofr30'].shift(), 'green', 
-                                    np.where(sofr30Df['sofr30'] < sofr30Df['sofr30'].shift(), 'red', 'gray'))
-            arrowHtml30 = [f'<span style="color:{color}">{arrow}</span>' for arrow, color in zip(sofr30Df['30-day Arrow'], arrowColor30)]
-            sofr30Df['30-day Arrow'] = arrowHtml30
 
             sofr90Df = pd.DataFrame(USEconomy.sofr90Data)
             sofr90Df.index = pd.to_datetime(sofr90Df.index)
             sofr90Df.columns = ['sofr90']
-            sofr90Df['90-day Arrow'] = np.where(sofr90Df['sofr90'] > sofr90Df['sofr90'].shift(), '▲', 
-                                                np.where(sofr90Df['sofr90'] < sofr90Df['sofr90'].shift(), '▼', '▬'))
-            arrowColor90 = np.where(sofr90Df['sofr90'] > sofr90Df['sofr90'].shift(), 'green', 
-                                    np.where(sofr90Df['sofr90'] < sofr90Df['sofr90'].shift(), 'red', 'gray'))
-            arrowHtml90 = [f'<span style="color:{color}">{arrow}</span>' for arrow, color in zip(sofr90Df['90-day Arrow'], arrowColor90)]
-            sofr90Df['90-day Arrow'] = arrowHtml90
 
             sofr180Df = pd.DataFrame(USEconomy.sofr180Data)
             sofr180Df.index = pd.to_datetime(sofr180Df.index)
             sofr180Df.columns = ['sofr180']
-            sofr180Df['180-day Arrow'] = np.where(sofr180Df['sofr180'] > sofr180Df['sofr180'].shift(), '▲', 
-                                                  np.where(sofr180Df['sofr180'] < sofr180Df['sofr180'].shift(), '▼', '▬'))
-            arrowColor180 = np.where(sofr180Df['sofr180'] > sofr180Df['sofr180'].shift(), 'green', 
-                                     np.where(sofr180Df['sofr180'] < sofr180Df['sofr180'].shift(), 'red', 'gray'))
-            arrowHtml180 = [f'<span style="color:{color}">{arrow}</span>' for arrow, color in zip(sofr180Df['180-day Arrow'], arrowColor180)]
-            sofr180Df['180-day Arrow'] = arrowHtml180
 
             fig = go.Figure(data = go.Scatter(x = sofrDf.index, y = sofrDf['sofr']))
 
             fig.update_layout(xaxis_title = 'Date', 
                               title = 'Secured Overnight Financing Rate')
-            st.subheader(f"Latest Secured Overnight Financing Rate: {sofrDf['sofr'].iloc[-1]:.2f}%")
+            st.markdown(f"### Latest Secured Overnight Financing Rate: {sofrDf['sofr'].iloc[-1]:.2f}% \
+                        {StockAnalyzer.arrow_change(sofrDf, 'sofr')}",
+                        unsafe_allow_html = True)
             sofrCol1, sofrCol2, sofrCol3 = st.columns([3, 3, 3])
-            sofrCol1.markdown(f"30-day Average: {sofr30Df['sofr30'].iloc[-1]:.2f}% {sofr30Df['30-day Arrow'].iloc[-1]}", 
+            sofrCol1.markdown(f"30-day Average: {sofr30Df['sofr30'].iloc[-1]:.2f}% {StockAnalyzer.arrow_change(sofr30Df, 'sofr30')}", 
                               unsafe_allow_html = True)
-            sofrCol2.markdown(f"90-day Average: {sofr90Df['sofr90'].iloc[-1]:.2f}% {sofr90Df['90-day Arrow'].iloc[-1]}", 
+            sofrCol2.markdown(f"90-day Average: {sofr90Df['sofr90'].iloc[-1]:.2f}% {StockAnalyzer.arrow_change(sofr90Df, 'sofr90')}", 
                               unsafe_allow_html = True)
-            sofrCol3.markdown(f"180-day Average: {sofr180Df['sofr180'].iloc[-1]:.2f}% {sofr180Df['180-day Arrow'].iloc[-1]}", 
+            sofrCol3.markdown(f"180-day Average: {sofr180Df['sofr180'].iloc[-1]:.2f}% {StockAnalyzer.arrow_change(sofr180Df, 'sofr180')}", 
                               unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
@@ -153,7 +150,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'USD to EURO')
-            st.subheader(f"Latest USD to EURO Exchange Rate: {usdEuroDf['exchange_rate'].iloc[-1]:.3f}")
+            st.markdown(f"### Latest USD to EURO Exchange Rate: {usdEuroDf['exchange_rate'].iloc[-1]:.3f} EUR\
+                        {StockAnalyzer.arrow_change(usdEuroDf, 'exchange_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
         if usdCad:
@@ -164,7 +163,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'USD to CAD')
-            st.subheader(f"Latest USD to CAD Exchange Rate: {usdCadDf['exchange_rate'].iloc[-1]:.3f} CAD")
+            st.markdown(f"### Latest USD to CAD Exchange Rate: {usdCadDf['exchange_rate'].iloc[-1]:.3f} CAD \
+                        {StockAnalyzer.arrow_change(usdCadDf, 'exchange_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
         
         if usdInr:
@@ -175,7 +176,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'USD to INR')
-            st.subheader(f"Latest USD to INR Exchange Rate: {usdInrDf['exchange_rate'].iloc[-1]:.3f} INR")
+            st.markdown(f"### Latest USD to INR Exchange Rate: {usdInrDf['exchange_rate'].iloc[-1]:.3f} INR \
+                        {StockAnalyzer.arrow_change(usdInrDf, 'exchange_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
         
         if usdYen:
@@ -186,7 +189,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'USD to YEN')
-            st.subheader(f"Latest USD to YEN Exchange Rate: {usdYenDf['exchange_rate'].iloc[-1]:.3f} YEN")
+            st.markdown(f"### Latest USD to YEN Exchange Rate: {usdYenDf['exchange_rate'].iloc[-1]:.3f} YEN \
+                        {StockAnalyzer.arrow_change(usdYenDf, 'exchange_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
         if usdRmb:
@@ -197,7 +202,9 @@ class USEconomy:
 
             fig.update_layout(xaxis_title = 'Date',
                               title = 'USD to RMB')
-            st.subheader(f"Latest USD to RMB Exchange Rate: {usdRmbDf['exchange_rate'].iloc[-1]:.3f} RMB")
+            st.markdown(f"### Latest USD to RMB Exchange Rate: {usdRmbDf['exchange_rate'].iloc[-1]:.3f} RMB \
+                        {StockAnalyzer.arrow_change(usdRmbDf, 'exchange_rate')}",
+                        unsafe_allow_html = True)
             st.plotly_chart(fig, use_container_width = True)
 
     def mortgage_rates() -> None:
@@ -216,8 +223,12 @@ class USEconomy:
         fig.update_layout(title = 'Fixed Mortgage Rates',
                           yaxis = dict(title = 'Rate'),
                           xaxis = dict(title = 'Date'))
-        st.subheader(f"Latest 15 Year Fixed Rate: {mortgage15Df['mort15'].iloc[-1]:.2f}%")
-        st.subheader(f"Latest 30 Year Fixed Rate: {mortgage30Df['mort30'].iloc[-1]:.2f}%")
+        st.markdown(f"### Latest 15 Year Fixed Rate: {mortgage15Df['mort15'].iloc[-1]:.2f}% \
+                    {StockAnalyzer.arrow_change(mortgage15Df, 'mort15')}", 
+                    unsafe_allow_html = True)
+        st.markdown(f"### Latest 30 Year Fixed Rate: {mortgage30Df['mort30'].iloc[-1]:.2f}% \
+                    {StockAnalyzer.arrow_change(mortgage30Df, 'mort30')}",
+                    unsafe_allow_html = True)
         st.plotly_chart(fig, use_container_width = True)
         
     def economy_chooser() -> None:
@@ -233,3 +244,4 @@ class USEconomy:
         selectedEconomyFunction = economyOptions.get(selectedEconomyOption)
         if selectedEconomyFunction:
             selectedEconomyFunction()
+
