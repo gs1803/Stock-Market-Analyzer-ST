269b1983974b520acdf80f8ee357063937d62e75
Guhan Sivakumar <98928448+gs1803@users.noreply.github.com>
2024-05-25 11:34:27 -0500

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     main()
         # Command to get the pybind11 includes
     # command = ["python", "-m", "pybind11", "--includes"]
-    command = ["pip", "list"]
+    command = ["find", "/", "-type", "f", "-name", "activate", "-print"]
 
     # Execute the command
     try:
