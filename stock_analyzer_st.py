03b4fef7b5e20f6e74f7cb904f8ab12c00229a10
Guhan Sivakumar <98928448+gs1803@users.noreply.github.com>
2024-05-28 21:31:16 -0500

    Update stock_analyzer_st.py

--- stock_analyzer_st.py
+++ stock_analyzer_st.py
@@ -27,7 +27,7 @@ class StockAnalyzer:
         except:
             self.mktCap = ('')
             
-        self.dayStock = yf.download(titleStock, period = '7d', progress = False)
+        self.dayStock = yf.download(titleStock, period = '5d', progress = False)
         self.etNow = datetime.now(pytz.timezone('US/Eastern')).date()
         self.config = {'displaylogo': False, 'modeBarButtonsToAdd': ['drawline', 'drawopenpath', 'eraseshape']}
         
