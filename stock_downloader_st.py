8d5fc77c6b6426e89c10a7155c98d0cd97e22eb8
Guhan Sivakumar <98928448+gs1803@users.noreply.github.com>
2025-04-08 18:52:35 -0500

    Update stock_downloader_st.py

--- stock_downloader_st.py
+++ stock_downloader_st.py
@@ -11,31 +11,31 @@ def download_stock_data(userStock, startDate, endDate):
         if startDate.isoweekday() == 6:
             start = startDate - timedelta(days = 1)                  
             end = endDate - timedelta(days = 1)
-            inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False)
+            inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False, auto_adjust=False)
         elif startDate.isoweekday() == 7:
             start = startDate - timedelta(days = 2)                    
             end = endDate - timedelta(days = 2)
-            inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False)
+            inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False, auto_adjust=False))
         elif etNow - startDate.date() >= timedelta(days = 30):
-            inputStock = yf.download(f"{userStock}", start, end, progress = False)
+            inputStock = yf.download(f"{userStock}", start, end, progress = False, auto_adjust=False))
         else:
-            inputStock = yf.download(f"{userStock}", startDate, endDate, interval = '1m', progress = False)
+            inputStock = yf.download(f"{userStock}", startDate, endDate, interval = '1m', progress = False, auto_adjust=False))
     elif dateDiff == 1:
         if startDate.isoweekday() == 6 or startDate.isoweekday() == 7 or endDate.isoweekday() - 1 == 6 or endDate.isoweekday() - 1 == 7:
             if startDate.isoweekday() == 5:
                 end = endDate - timedelta(days = 1)
-                inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False)
+                inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False, auto_adjust=False))
             else:
                 start = startDate - timedelta(days = 1)                    
                 end = endDate - timedelta(days = 1)
-                inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False)
+                inputStock = yf.download(f"{userStock}", start, end, interval = '1m', progress = False, auto_adjust=False))
         else:
-            inputStock = yf.download(f"{userStock}", startDate, endDate, progress = False)
+            inputStock = yf.download(f"{userStock}", startDate, endDate, progress = False, auto_adjust=False))
     elif dateDiff == 2 and startDate.isoweekday() == 5:
         end = endDate - timedelta(days = 2)
-        inputStock = yf.download(f"{userStock}", startDate, end, interval = '1m', progress = False)
+        inputStock = yf.download(f"{userStock}", startDate, end, interval = '1m', progress = False, auto_adjust=False))
     else:
-        inputStock = yf.download(f"{userStock}", startDate, endDate, progress = False)
+        inputStock = yf.download(f"{userStock}", startDate, endDate, progress = False, auto_adjust=False))
 
     inputStock = inputStock.xs(f"{userStock}", axis=1, level="Ticker").reset_index()
     return inputStock
